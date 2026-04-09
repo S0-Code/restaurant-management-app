f6b0c3fb7a3cf4944a2c148161a607ba49ce7f85
Sofian Ben Aissa <1211sobenaissa@student.epfc.eu>
2026-04-09 14:43:59 +0200

    feat(user_constrains) : check Hash is in Security & rule can not change to do in feat_cn_not_change_this

--- backend/scripts/user_constraints.sql
+++ backend/scripts/user_constraints.sql
@@ -1,9 +1,14 @@
 set search_path to public, auth;
 
-alter table users add constraint full_name_min_length
+/* Le nom complet doit avoir une longueur d'au moins 3 caractères.*/
+
+    alter table users add constraint full_name_min_length
     check ( length(trim(full_name)) >= 3  );
 
 
+/* Le mot de passe doit avoir au minimum une longueur de 8 caractères,
+   doit contenir au moins un chiffre, une lettre majuscule,
+   une lettre minuscule et un caractère spécial parmi les suivants : ,;.:!?/$%&@#. */
 
 alter table users add constraint check_password_format
     check (length(trim(password)) >= 8
@@ -11,3 +16,11 @@ alter table users add constraint check_password_format
         AND password ~ '[A-Z]'
         AND password ~ '[0-9]'
         AND password ~ '[,;.:!?/$%&@#]');
+
+/* Le mot de passe doit être stocké de manière sécurisée (hachage + sel). */
+    /*  function auth.encrypt_pass() est déjà dans Security
+
+
+/*On ne peut pas modifier le rôle d'un utilisateur.*/
+        Va être fait dans Can_not_change_this
+     */
