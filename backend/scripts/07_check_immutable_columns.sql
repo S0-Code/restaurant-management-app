081cee88fbf2f421ee9a7e9d2a2619d37bcf68a8
Ismaïl Bentatou <1507isbentatou@student.epfc.eu>
2026-04-16 17:18:19 +0200

    feat(services_constraints) : check constraints on services table (end_time after start_time, column restaurant immutable)

--- backend/scripts/07_check_immutable_columns.sql
+++ backend/scripts/07_check_immutable_columns.sql
@@ -34,15 +34,23 @@ $$ language plpgsql;
 
 drop trigger if exists trg_prevent_role_update on users;
 create trigger trg_prevent_role_update
-    before update on users
+    before update of role on users
     for each row
     execute procedure prevent_column_update('role');
 
 
 /*On ne peut pas modifier le restaurant d'une table. */
 
-drop  trigger if exists trg_prevent_restaurant_update on tables;
-create trigger trg_prevent_restaurant_update
-    before update on tables
+drop  trigger if exists trg_prevent_restaurant_of_table_update on tables;
+create trigger trg_prevent_restaurant_of_table_update
+    before update of restaurant on tables
     for each row
     execute procedure prevent_column_update('restaurant');
+
+/*On ne peut pas modifier le restaurant d'un service.*/
+
+drop  trigger if exists trg_prevent_restaurant_of_service_update on services;
+create trigger trg_prevent_restaurant_of_service_update
+    before update of restaurant on services
+    for each row
+execute procedure prevent_column_update('restaurant');
