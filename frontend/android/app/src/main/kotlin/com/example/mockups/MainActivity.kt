7e4e091acf66ca621298ad91c08ccf6f280f0c0f
Ismaïl Bentatou <1507isbentatou@student.epfc.eu>
2026-05-15 11:43:45 +0200

    Revert "feat(frontend): integrate UI mockups and update Flutter configuration"
    This reverts commit d388d5331c91146a040b57cf4db8968b1346c012.

--- frontend/android/app/src/main/kotlin/com/example/mockups/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mockups
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/android/app/src/main/kotlin/com/example/prbd_2526_c05/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.prbd_2526_c05
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
